42e316b5ab4c6efa338745748ff08976feb81e34
Cas Li <tina.li003@gmail.com>
2022-07-27 21:50:36 -0700

    getting spotify sdk connected/authorized to spotify app

--- Vwitter/AppDelegate.m
+++ Vwitter/AppDelegate.m
@@ -6,9 +6,12 @@
 //
 
 #import <Parse/Parse.h>
-#import <SpotifyiOS/SpotifyiOS.h>
 
 #import "AppDelegate.h"
+#import "VWHelpers.h"
+
+static NSString * const SpotifyClientID = @"e4185723643e4db9bcf48af28e078cff";
+static NSString * const SpotifyRedirectURLString = @"vwitter://callback/";
 
 @interface AppDelegate ()
 
@@ -25,26 +28,68 @@
 
         configuration.applicationId = [dict objectForKey: @"applicationId"];
         configuration.clientKey = [dict objectForKey: @"clientKey"];
-        configuration.server = @"http://localhost:1337/parse";
-//        configuration.server = @"https://parseapi.back4app.com";
+//        configuration.server = @"http://localhost:1337/parse";
+        configuration.server = @"https://parseapi.back4app.com";
     }];
 
     [Parse initializeWithConfiguration:config];
     
+//    NSString *_Nullable previouslySavedAccessToken = CAST_TO_CLASS_OR_NIL([NSUserDefaults.standardUserDefaults objectForKey:@"spotify_token"], NSString);
+//    if (previouslySavedAccessToken) {
+//        // user previously authenticated with spotify
+//        self.appRemote.connectionParameters.accessToken = previouslySavedAccessToken;
+//    }
+    
+    // init app remote
+    
+    SPTConfiguration *configuration =
+        [[SPTConfiguration alloc] initWithClientID:SpotifyClientID redirectURL:[NSURL URLWithString:SpotifyRedirectURLString]];
+    
+    self.appRemote = [[SPTAppRemote alloc] initWithConfiguration:configuration logLevel:SPTAppRemoteLogLevelDebug];
+    
+    BOOL spotifyInstalled = [self.appRemote authorizeAndPlayURI:@""];
+    if (!spotifyInstalled) {
+        /*
+        * The Spotify app is not installed.
+        * Use SKStoreProductViewController with [SPTAppRemote spotifyItunesItemIdentifier] to present the user
+        * with a way to install the Spotify app.
+        */
+        NSString *adjustUrl = @"https://app.adjust.com/bdyga9?campaign=com.cyli.Vwitter";
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:adjustUrl]];
+        [request setValue:@"spotify_campaign_user_agent" forHTTPHeaderField:@"User-Agent"];
+
+        [[[NSURLSession sharedSession] dataTaskWithRequest:request
+                                         completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        }] resume];
+        
+        NSString *url = @"https://itunes.apple.com/app/spotify-music/id324684580?mt=8";
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:url] options:@{} completionHandler:^(BOOL success) {
+            if (success) {
+                NSLog(@"Opened url");
+            }
+        }];
+
+    }
+
+    
     return YES;
 }
 
-//- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-//{
-//    NSDictionary *params = [self.appRemote authorizationParametersFromURL:url];
-//    NSString *token = params[SPTAppRemoteAccessTokenKey];
-//    if (token) {
-//        self.appRemote.connectionParameters.accessToken = token;
-//    } else if (params[SPTAppRemoteErrorDescriptionKey]) {
-//        NSLog(@"%@", params[SPTAppRemoteErrorDescriptionKey]);
-//    }
-//    return YES;
-//}
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+    NSDictionary *params = [self.appRemote authorizationParametersFromURL:url];
+    NSString *token = params[SPTAppRemoteAccessTokenKey];
+    if (token) {
+        self.appRemote.connectionParameters.accessToken = token;
+        
+        // save the token if needed
+        [NSUserDefaults.standardUserDefaults setObject:token forKey:@"spotify_token"];
+        
+    } else if (params[SPTAppRemoteErrorDescriptionKey]) {
+        NSLog(@"%@", params[SPTAppRemoteErrorDescriptionKey]);
+    }
+    return YES;
+}
 
 
 #pragma mark - UISceneSession lifecycle
--- Vwitter/View Controllers/SpotifyViewController.m
+++ Vwitter/View Controllers/SpotifyViewController.m
@@ -8,9 +8,11 @@
 
 #import "SpotifyViewController.h"
 #import "UIViewController+ErrorAlertPresenter.h"
+#import "AppDelegate.h"
+#import "VWHelpers.h"
 
 static NSString * const SpotifyClientID = @"e4185723643e4db9bcf48af28e078cff";
-static NSString * const SpotifyRedirectURLString = @"vwitter://spotify-login-callback";
+static NSString * const SpotifyRedirectURLString = @"vwitter://callback/";
 
 @interface SpotifyViewController ()
 
@@ -19,6 +21,9 @@ static NSString * const SpotifyRedirectURLString = @"vwitter://spotify-login-cal
 @implementation SpotifyViewController
 
 - (void)viewDidLoad {
+    
+    AppDelegate *appDelegate = CAST_TO_CLASS_OR_NIL(UIApplication.sharedApplication.delegate, AppDelegate);
+    
     /*
      This configuration object holds your client ID and redirect URL.
      */
@@ -31,46 +36,75 @@ static NSString * const SpotifyRedirectURLString = @"vwitter://spotify-login-cal
                                                                     delegate:self];
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    __weak typeof(self) weakSelf = self;
-    [SPTAppRemote checkIfSpotifyAppIsActive:^(BOOL active) {
-        typeof(self) strongSelf = weakSelf;
-        if (!strongSelf) {
-            NSLog(@"I got killed!");
-            return;
-        }
-        if (active) {
-            // Prompt the user to connect Spotify here
-            [self presentErrorMessageWithTitle:@"Spotify is installed" message:@"Spotify is installed"];
-        }
-        else {
-            [self presentErrorMessageWithTitle:@"Spotify not installed" message:@"Spotify not installed"];
-        }
-    }];
+//    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"spotify:"]]) {
+//        [self presentErrorMessageWithTitle:@"Spotify is installed" message:@"Spotify is installed"];
+//    }
+//    else {
+//        [self presentErrorMessageWithTitle:@"Spotify not installed" message:@"Spotify not installed"];
+//    }
+//    __weak typeof(self) weakSelf = self;
+//    [SPTAppRemote checkIfSpotifyAppIsActive:^(BOOL active) {
+//        typeof(self) strongSelf = weakSelf;
+//        if (!strongSelf) {
+//            NSLog(@"I got killed!");
+//            return;
+//        }
+//        if (active) {
+//            // Prompt the user to connect Spotify here
+//            [self presentErrorMessageWithTitle:@"Spotify is installed" message:@"Spotify is installed"];
+//        }
+//        else {
+////            [self presentErrorMessageWithTitle:@"Spotify not installed" message:@"Spotify not installed"];
+//            if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"spotify:"]]) {
+//                [self presentErrorMessageWithTitle:@"Spotify is installed" message:@"Spotify is installed"];
+//            }
+//            else {
+//                [self presentErrorMessageWithTitle:@"Spotify not installed" message:@"Spotify not installed"];
+//            }
+//        }
+//    }];
+    
+//    appDelegate.appRemote.delegate = self;
+//    [appDelegate.appRemote connect];
 }
 
+- (IBAction)didTapConnect:(id)sender {
+    /*
+     Scopes let you specify exactly what types of data your application wants to
+     access, and the set of scopes you pass in your call determines what access
+     permissions the user is asked to grant.
+     For more information, see https://developer.spotify.com/web-api/using-scopes/.
+     */
+    SPTScope scope = SPTUserLibraryReadScope | SPTPlaylistReadPrivateScope;
+
+    /*
+     Start the authorization process. This requires user input.
+     */
+    if (@available(iOS 11, *)) {
+        // Use this on iOS 11 and above to take advantage of SFAuthenticationSession
+        [self.sessionManager initiateSessionWithScope:scope options:SPTDefaultAuthorizationOption];
+    } else {
+        // Use this on iOS versions < 11 to use SFSafariViewController
+        NSLog(@"iOS version too old");
+    }
+}
 #pragma mark - SPTSessionManagerDelegate
 
 - (void)sessionManager:(SPTSessionManager *)manager didInitiateSession:(SPTSession *)session
 {
-//    [self presentAlertControllerWithTitle:@"Authorization Succeeded"
-//                                  message:session.description
-//                              buttonTitle:@"Nice"];
+
     [self presentErrorMessageWithTitle:@"Auth succeeded" message:@"Nice"];
 }
 
 - (void)sessionManager:(SPTSessionManager *)manager didFailWithError:(NSError *)error
 {
-//    [self presentAlertControllerWithTitle:@"Authorization Failed"
-//                                  message:error.description
-//                              buttonTitle:@"Bummer"];
+
     [self presentErrorMessageWithTitle:@"Auth failed" message:@"rip"];
 }
 
 - (void)sessionManager:(SPTSessionManager *)manager didRenewSession:(SPTSession *)session
 {
-//    [self presentAlertControllerWithTitle:@"Session Renewed"
-//                                  message:session.description
-//                              buttonTitle:@"Sweet"];
+
     [self presentErrorMessageWithTitle:@"Auth renewed" message:@"Nice"];
 }
 
